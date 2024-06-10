8063d02e64017033a68885603f5af65e81ec1e14
Tân <leetaan1902@gmail.com>
2024-06-10 11:37:06 +0700

    learn v1

--- client/social-media/src/App.js
+++ client/social-media/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+  import logo from './logo.svg'
+  import { useState } from 'react';
+  import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <a>hehehee</a>
-    </div>
-  );
-}
+  const orders = [100,200,300];
 
-export default App;
+  function App() {
+  
+    const [count, setCount] = useState(()=>{
+      const total = orders.reduce((total, cur)=>{
+        return (total + cur);
+      })
+      return total;
+    });
+    function handle(){
+      // setCount(count +1);
+      setCount(prevState => prevState + 1);
+    }
+
+    return (
+      <div className="App">
+      <h1>{count}</h1>
+      <button onClick={handle}>nhấn vào</button>
+      </div>
+    );
+  }
+
+  export default App;
