b502a15ff296aeadc0881e368fdcc21c6a4aa4c2
Tân <leetaan1902@gmail.com>
2024-12-16 19:55:33 +0700

    111

--- server/social-media/src/main/java/org/example/socialmedia/authentication/controller/TestController.java
+++ server/social-media/src/main/java/org/example/socialmedia/authentication/controller/TestController.java
@@ -55,6 +55,7 @@ public class TestController {
         System.out.println("test2");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test3");
         return new ResponseSuccess(HttpStatus.CREATED, "Success", 1);
     }
 
